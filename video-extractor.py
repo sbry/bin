bd640d76b4dbe9aba2052e581c1b116694caf9c6
Justin Salisbury <justin@salisbury.de>
2020-10-25 19:13:36 +0100

    now with nice logging

--- video-extractor.py
+++ video-extractor.py
@@ -1,21 +1,30 @@
-import sys, time, os, re
-# no pathlib means no python3 i guess.
+import sys, time, os, re, logging
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(levelname)s:%(message)s',)
+
 try:
     from pathlib import Path
 except ImportError:
-    print("Python 3 is required.")
+    logging.error("no pathlib means no python3, but python 3 is required.")
     sys.exit()
-    
-from shutil import copyfile
+
+from shutil import copyfile    
+
 
 ##
 # always the same target
 home = Path(os.getenv("HOME"))
 target = home / "Videos"
 
+logging.info("target dir is %s", target)
+
 ##
 # different sources
 source = home / "Pictures"
+
+logging.info("source is %s", source)
 
 def get_video_files(p):
     ext = {'.avi', '.mp4', '.vob', '.mov'}
@@ -23,21 +32,30 @@ def get_video_files(p):
         if path.suffix.lower() in ext:
             yield(path)
 
+
+            
 def process_video_file(filename):
+    logging.info('Processing Source %s', filename)  
+    # we make a relative path and prepend the target and we do that
+    # because very often the directory-name contains important
+    # meta-information, and also the video-filenames might be the same.
     target_filename = target / filename.resolve().relative_to(source)
-    # return target_filename
+    logging.info('Target %s', target_filename)  
+    # do not overwrite
     if target_filename.exists():
-        return target_filename
+        logging.info('Target already exists %s, bailing out', target_filename)  
+        return
+
+
     target_filename.parent.mkdir(parents=True, exist_ok=True)
-    # cant rename beyond filesystems
+    logging.info('Before the copying of %s -> %s', filename, target_filename)  
     copyfile(filename, target_filename)
     filename.unlink(missing_ok=True)
-    # filename.rename(target_filename)
-    return target_filename
+    logging.info('%s complete and %s unlinked', target_filename, filename)  
+
+    return
 
 if __name__ == "__main__":
     for f in get_video_files(source):
-        print(f)
-        t = process_video_file(f)
-        print(t)
-    pass
+        process_video_file(f)
+
