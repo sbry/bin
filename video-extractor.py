ed8759e6d50b2eaaa844fe58b1ed922e94af5bb7
Justin Salisbury <justin@salisbury.de>
2020-10-26 10:53:02 +0100

    now it works perfectly

--- video-extractor.py
+++ video-extractor.py
@@ -1,10 +1,13 @@
 import sys, time, os, re, logging, shutil
+###
+## The point is that this stuff runs on windows linux mac like plain
+# vanilla python
 ##
-# We use pathlib
+# We like pathlib
 try:
     from pathlib import Path
 except ImportError:
-    logging.error("no pathlib means no python3, but python 3 is required.")
+    logging.error("no pathlib means no python3, but python 3 is required")
     sys.exit()
 
 DEBUG = False
@@ -23,7 +26,7 @@ try:
         target = Path(sys.argv[2])
     except IndexError:
         target = source.parent  / "Videos"
-        logging.warning('target omitted, it is now %s', target)
+        logging.warning('target is now %s', target)
 except IndexError:
     logging.error('please provide at least a source-dir')
     sys.exit()
@@ -33,8 +36,6 @@ logging.debug("target dir is %s", target)
 
 if not DEBUG:
     target.mkdir(parents=True, exist_ok=True)
-
-
     
 def get_video_files(p):
     ext = {'.avi', '.mp4', '.vob', '.mov'}
@@ -43,18 +44,18 @@ def get_video_files(p):
             yield(path)
 
 def get_target_filename(source_filename):
+    """make a relative path and prepend the target, because very often
+
+    the directory-name contains important meta-information, and also the
+    video-filenames might be the same.  so
+    /source/test-name/video-file.mov becomes
+    /target/test-name/video-file.mov
+    """
     target_filename = target / source_filename.resolve().relative_to(source)
     logging.debug('%s -> %s', source_filename, target_filename)
     return target_filename
     
 def process_video_file(source_filename):
-    """we make a relative path and prepend the target and we do that
-
-    because very often the directory-name contains important
-    meta-information, and also the video-filenames might be the same.
-    so /source/test-name/video-file.mov becomes
-    /target/test-name/video-file.mov
-    """
     target_filename = get_target_filename(source_filename)
     if target_filename.exists():
         logging.info('target already exists %s, bailing out', target_filename)  
@@ -66,15 +67,16 @@ def process_video_file(source_filename):
         return
 
     logging.info('%s -> %s', source_filename, target_filename)
+    
+    target_filename.parent.mkdir(parents=True, exist_ok=True)
     try:
         source_filename.rename(target_filename)
         logging.info('renamed')  
     except:
-        target_filename.parent.mkdir(parents=True, exist_ok=True)
         shutil.copyfile(source_filename, target_filename)
         source_filename.unlink()
-        logging.info('copied and unlinked (filesystem boundaries)')  
-    return
+        logging.info('copied and unlinked')  
+
 
 if __name__ == "__main__":
     for f in get_video_files(source):
